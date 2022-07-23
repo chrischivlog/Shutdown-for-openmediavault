202a188494c0a10868e055a154990ba417bd6972
Christopher Oneisz <coneisz@chrischi.one>
2022-07-23 17:35:52 +0200

    Update shutdown.py
    Added
    + support for more than one ip

--- shutdown.py
+++ shutdown.py
@@ -1,14 +1,28 @@
 #!/usr/bin/env python
 
+from logging import shutdown
 import os
+from time import process_time_ns
 
-ips = ['192.168.178.41','192.168.178.40']
+ips = ['192.168.178.82','192.168.178.40','192.168.178.41','192.168.178.74']
+off_devices = 0  # set offline devcies to 0
+sumips = len(ips)  # count all ips in array
 
-for i in ips: 
-   response = os.system("ping -c 1 " + i)
 
-if response == 0:
-    print("erreichbar")
-else:
-    print("nicht erreichbar")
+for i in ips:
+        response_ping = os.system("ping -c 1 " + i)
+        print(response_ping)
+        
+        if response_ping == 512:
+                off_devices += 1
+
+                counted_off_devices = off_devices
+                print('Geräte offline:', counted_off_devices, '; IPs gezählt:', sumips)
+
+
+if counted_off_devices == sumips:
+    print('shutdown')
     os.system("shutdown -h now")
+
+else:
+    print('stay online')
